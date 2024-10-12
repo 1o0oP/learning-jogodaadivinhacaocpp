820022e26486d7358cd23354d7b508386d12a1f8
lsilvpin <lsilvpin@gmail.com>
2024-10-12 14:16:04 -0300

    Lógica de dificuldade implementada

--- main.cpp
+++ main.cpp
@@ -9,12 +9,18 @@ int main()
     cout << "*** Jogo da Adivinhação ***" << endl;
     cout << "***************************" << endl;
 
+    cout << "Escolha o seu nível de dificuldade: " << endl;
+    cout << "Fácil (F), Médio (M) ou Difícil (D)" << endl;
+    char dificuldade;
+    cin >> dificuldade;
+
     const int NUMERO_SECRETO = generateRandomSecretNumber();
     bool acertou = false;
     int tentativas = 0;
+    int tentativasMaximas = getTentativasMaximas(dificuldade);
     double pontos = 1000.0;
 
-    while (!acertou)
+    for (int i = 1; i < tentativasMaximas; i++)
     {
         int chute;
         cout << "Digite o seu chute: ";
@@ -28,17 +34,26 @@ int main()
         pontos -= pontosPerdidos(chute, NUMERO_SECRETO);
     }
 
-    printGameOver(tentativas, pontos);
+    printGameOver(tentativas, pontos, acertou);
     return 0;
 }
 
-void printGameOver(int tentativas, double pontos)
+void printGameOver(int tentativas, double pontos, bool acertou)
 {
     cout << "Fim de jogo!" << endl;
-    cout << "Você acertou o número secreto em " << tentativas << " tentativas!" << endl;
-    cout.precision(2);
-    cout << fixed;
-    cout << "Sua pontuação foi de " << pontos << " pontos!" << endl;
+    if (acertou)
+    {
+        cout << "Você acertou o número secreto em " << tentativas << " tentativas!" << endl;
+        cout.precision(2);
+        cout << fixed;
+        cout << "Sua pontuação foi de " << pontos << " pontos!" << endl;
+    }
+    else
+    {
+        cout << "Você atingiu o limite de tentativas!" << endl;
+        cout << "Sua pontuação foi de " << pontos << " pontos!" << endl;
+        cout << "Tente novamente!" << endl;
+    }
 }
 
 void printFeedback(int chute, const int NUMERO_SECRETO)
@@ -75,4 +90,29 @@ double pontosPerdidos(int chute, const int NUMERO_SECRETO)
 bool isMatch(int chute, const int NUMERO_SECRETO)
 {
     return chute == NUMERO_SECRETO;
+}
+
+int getTentativasMaximas(char dificuldade)
+{
+    int tentativasMaximas;
+    switch (dificuldade)
+    {
+        case 'F':
+            tentativasMaximas = 15;
+            break;
+        case 'M':
+            tentativasMaximas = 10;
+            break;
+        case 'D':
+            tentativasMaximas = 5;
+            break;
+        default:
+            throw "Dificuldade inválida!";
+    }
+    return tentativasMaximas;
+}
+
+bool tentativasMaximasAtingidas(int tentativas, int tentativasMaximas)
+{
+    return tentativas >= tentativasMaximas;
 }
