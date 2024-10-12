de811210a481ef1e747f0dffb84735eedcba5944
lsilvpin <lsilvpin@gmail.com>
2024-10-12 13:06:27 -0300

    Variável fixa transformada em constante

--- main.cpp
+++ main.cpp
@@ -8,9 +8,9 @@ int main()
     cout << "*** Jogo da Adivinhação ***" << endl;
     cout << "***************************" << endl;
 
-    int numeroSecreto = 42;
+    const int NUMERO_SECRETO = 42;
 
-    cout << "O número secreto é " << numeroSecreto << endl;
+    cout << "O número secreto é " << NUMERO_SECRETO << endl;
 
     int chute;
     cout << "Digite o seu chute: ";
@@ -18,13 +18,13 @@ int main()
 
     cout << "O valor do seu chute é: " << chute << endl;
 
-    bool acertou = chute == numeroSecreto;
+    bool acertou = chute == NUMERO_SECRETO;
 
     if (acertou)
     {
         cout << "Parabéns! Você acertou o número secreto!" << endl;
     }
-    else if (chute > numeroSecreto)
+    else if (chute > NUMERO_SECRETO)
     {
         cout << "Seu chute foi maior que o número secreto!" << endl;
     }
