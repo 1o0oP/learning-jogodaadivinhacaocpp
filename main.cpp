a5e5cb150b2247428046bd1a9804468cbcd3e4ae
lsilvpin <lsilvpin@gmail.com>
2024-10-12 12:30:23 -0300

    main() implementada

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "***************************" << std::endl;
+    std::cout << "*** Jogo da Adivinhação ***" << std::endl;
+    std::cout << "***************************" << std::endl;
+    return 0;
+}
